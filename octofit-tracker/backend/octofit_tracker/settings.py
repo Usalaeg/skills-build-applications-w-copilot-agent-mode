f98147dcaa0c5d4563c1cecabb37c908b20fa7b6
Usha <121151493+Usalaeg@users.noreply.github.com>
2025-12-15 12:17:00 +0000

    Update settings and URLs to support Codespace environment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-q7k13pxx9y=x(ri^1i53-uoallw&w=$_qga9*3@rldwp^try7c
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow Codespace URL and localhost
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
